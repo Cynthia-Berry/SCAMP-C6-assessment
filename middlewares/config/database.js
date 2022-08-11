19a08ca0cd912de61fe2c5869ce9e303a92f2dbd
Nwakaeme Cynthia Ebere <nwakaemecynthia@gmail.com>
2022-08-11 19:19:06 +0100

    fixed connection issue

--- middlewares/config/database.js
+++ middlewares/config/database.js
@@ -2,13 +2,19 @@ const {Sequelize} = require("sequelize");
 
 
 module.exports = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
-    dialect: process.env.POSTGRES_DIALECT,
-
-    pool: {
-        max: 5,
-        min:0,
-        acquire: 10000,
-        idle: 5000
-    },
+	host: process.env.POSTGRES_HOST,
+	dialect: process.env.POSTGRES_DIALECT,
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+		}
+	},
+	
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 10000,
+		idle: 5000
+	},
 });
